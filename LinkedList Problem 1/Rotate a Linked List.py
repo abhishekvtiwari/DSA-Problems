8f446bb853e59759da3d7f6d79400a4aea3a6392
Abhishek Tiwari <68350077+abhishekvtiwari@users.noreply.github.com>
2021-04-09 21:05:23 +0530

    Update Rotate a Linked List.py

--- LinkedList Problem 1/Rotate a Linked List.py
+++ LinkedList Problem 1/Rotate a Linked List.py
@@ -1,14 +1,45 @@
-list1= [1, 4, 6, 8, 7]
-def rotatearray(arr):
-    newarr = list()
-    for i in range(len(arr)):
-        if i == 0:
-            newarr.append(arr[-1])
-        else:
-            newarr.append(arr[i-1])
-    return newarr
-
-if __name__ == '__main__':
-    print(rotatearray(list1))
-
+class Node:
+  def __init__(self,data):
+    self.data=data
+    self.next=None
     
+class LinkedList:
+  def __init__(self):
+    self.head=None
+    self.c=0
+
+
+  def add_element(self,data):
+    self.c=0
+    new_node=Node(data)
+    if self.head is None:
+      self.head=new_node
+      return
+    current=self.head
+    while current.next:
+      self.c+=1
+      current=current.next
+    current.next=new_node
+
+  def rotate_by(self):
+    temp=Node(0)
+    sec_last=self.head
+    while sec_last.next.next:
+      sec_last=sec_last.next
+    temp=sec_last.next
+    sec_last.next=None
+    temp.next=self.head
+    self.head=temp
+  def Print(self):
+    current=self.head
+    while current:
+      print(current.data)
+      current=current.next
+# I Love You
+a=[1, 4, 6, 8, 7]
+b=LinkedList()
+for i in a:
+  b.add_element(i)
+
+b.rotate_by()
+b.Print()
